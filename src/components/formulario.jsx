9ec7a4d1b68893e87c480803c5d96aa426193fb4
BorisLeightonI <borisleighton@gmail.com>
2024-07-02 09:51:28 -0400

    Validación de campos en formulario

--- src/components/formulario.jsx
+++ src/components/formulario.jsx
@@ -1,4 +1,4 @@
-import { Field, Form, Formik } from "formik"
+import { ErrorMessage, Field, Form, Formik } from "formik"
 import { useContext, useEffect, useRef, useState } from "react"
 import './formulario.css';
 import { formContext } from "../contextos/context";
@@ -36,27 +36,50 @@ function Formulario() {
         .catch(err => console.error(err))
     }
   }
+  const handleValidar = (values)=>{
+    const errors = {};
+    if(!values.nombreCliente){
+      errors.nombreCliente = 'El nombre de cliente es requerido';
+    }
+    if(!values.fecha) errors.fecha = 'La fecha es requerida';
+    if(!values.hora) errors.hora = 'La hora es requerida';
+    if(!values.Descripcion) errors.Descripcion = 'La Descripción es requerida';
+    return errors;
+  } 
   return (
     <div className="form_container">
       <Formik initialValues={
         action===EDIT?{...cita}:{Descripcion: "", fecha: "", hora: "", nombreCliente: ""}
         } 
         onSubmit={handleSubmit} 
+        validate={handleValidar}
         enableReinitialize>
         <Form onClick={e => e.stopPropagation()}>
           <div className="nombre">
             <label htmlFor="">Nombre de Cliente</label>
             <Field placeholder='Nombre de Cliente' name='nombreCliente' disabled={action===SHOW}/>
+            <div className="error_message">
+              <ErrorMessage name="nombreCliente"/>
+            </div>
           </div>
           <div className="fecha">
             <label htmlFor="">Fecha</label>
             <Field placeholder='Fecha' name='fecha' type='date' disabled={action===SHOW}/>
+            <div className="error_message">
+              <ErrorMessage name="fecha"/>
+            </div>
           </div>
           <div className="hora">
             <label htmlFor="">Hora</label>
             <Field placeholder='Hora' name='hora' type='time' disabled={action===SHOW}/>
+            <div className="error_message">
+              <ErrorMessage name="hora"/>
+            </div>
           </div>
           <Field placeholder='Descripción' name='Descripcion' as="textArea" disabled={action===SHOW}/>
+          <div className="error_message">
+              <ErrorMessage name="Descripcion"/>
+            </div>
           {enviado&& 
           <p>
             {action===EDIT?'Editado correctamente':'Creado Correctamente'}
